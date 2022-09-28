5957f26c84acc8719661e2402141f175f6a85a76
Lao-Liu233 <laoliu735@gmail.com>
2022-09-28 21:34:43 +0800

    Update: Fix Database API & Add Tests

--- src/database.rs
+++ src/database.rs
@@ -62,31 +62,64 @@ pub mod database {
         db.flush().expect("TODO: panic message");
     }
 
-    pub fn get_twitter_users() -> Vec<String> {
+    pub fn get_twitter_users() -> Vec<(String, String)> {
         let options: Options = Options {
             create_if_missing: true,
             ..Options::default()
         };
         let mut db = DB::open("twitter_users", options).unwrap();
         let mut users = Vec::new();
-        for mut value in db.new_iter() {
-            let (_key, val) = value.next().unwrap();
-            users.push(String::from_utf8(val).unwrap().to_string());
+        let mut iter = db.new_iter().unwrap();
+        while iter.advance() {
+            let mut key: Vec<u8> = Vec::new();
+            let mut value: Vec<u8>= Vec::new();
+            if iter.current(&mut key, &mut value) {
+                users.push((String::from_utf8(key).unwrap().to_string(), String::from_utf8(value).unwrap().to_string()));
+            }
         }
         users
     }
 
-    pub fn get_pixiv_users() -> Vec<String> {
+    pub fn get_pixiv_users() -> Vec<(String, String)> {
         let options: Options = Options {
             create_if_missing: true,
             ..Options::default()
         };
         let mut db = DB::open("pixiv_users", options).unwrap();
         let mut users = Vec::new();
-        for mut value in db.new_iter() {
-            let (_key, val) = value.next().unwrap();
-            users.push(String::from_utf8(val).unwrap().to_string());
+        let mut iter = db.new_iter().unwrap();
+        while iter.advance() {
+            let mut key: Vec<u8> = Vec::new();
+            let mut value: Vec<u8>= Vec::new();
+            if iter.current(&mut key, &mut value) {
+                users.push((String::from_utf8(key).unwrap().to_string(), String::from_utf8(value).unwrap().to_string()));
+            }
         }
         users
+    }
+
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn add_del_twitter_user_test() {
+            use super::*;
+            add_twitter_user(&324391547, &"avogado6".to_string());
+            let users = get_twitter_users();
+            assert_eq!(users[0], ("324391547".to_string(), "avogado6".to_string()));
+            remove_twitter_user(&324391547);
+            let users = get_twitter_users();
+            assert_eq!(users.len(), 0);
+        }
+
+        #[test]
+        fn add_del_pixiv_user_test() {
+            use super::*;
+            add_pixiv_user(&101484832, &"Nakkar".to_string());
+            let users = get_pixiv_users();
+            assert_eq!(users[0], ("101484832".to_string(), "Nakkar".to_string()));
+            remove_pixiv_user(&101484832);
+            let users = get_pixiv_users();
+            assert_eq!(users.len(), 0);
+        }
     }
 }
